51e705da2587c812d544456e084a740388c21c8e
roncamposj <roncamposj@knights.ucf.edu>
2023-04-24 11:37:28 -0400

    cleaned up directory and added to README

---
+++ archived_code/train.py
@@ -0,0 +1,65 @@
+import torch
+import os
+from torchvision.models import vit_b_32
+from torchvision import transforms
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader
+from torch import nn
+from tqdm import tqdm
+
+BASE_DATA_DIR = os.path.join("data", "ePillID_data")
+NUM_EPOCHS = 25
+NUM_CLASSES = 4902
+BATCH_SIZE = 5
+
+from datasets import SingleImgPillID
+
+label_encoder = LabelEncoder()
+
+
+
+images_df = pd.read_csv(os.path.join(BASE_DATA_DIR, "all_labels.csv"))
+
+label_encoder.fit(images_df['label'])
+
+transform = transforms.Compose([
+  transforms.ToTensor(),
+  transforms.Resize((224, 224), antialias=True)
+])
+
+train_dataset = SingleImgPillID(df=images_df, label_encoder=label_encoder, transform=transform, train=True)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = vit_b_32(num_classes=NUM_CLASSES)
+
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss()
+
+model.to(device)
+
+for epoch in range(NUM_EPOCHS):
+  running_loss = 0.0
+  for item in tqdm(train_loader):
+    image = item['image'].to(device)
+    label = item['label'].to(device)
+    image_name = item['image_name']
+    is_ref = item['is_ref']
+
+    optimizer.zero_grad()
+    outputs: torch.Tensor = model(image)
+    
+    loss = criterion(outputs, label)
+    loss.backward()
+    optimizer.step()
+
+    running_loss += loss.item() * image.size(0)
+
+  print(f"epoch {epoch} loss = {running_loss / len(train_loader)}")
+
+
+
+
+
+  
--- train.py
+++ train.py
@@ -1,65 +1,60 @@
-import torch
 import os
-from torchvision.models import vit_b_32
-from torchvision import transforms
+from glob import glob
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import DataLoader
-from torch import nn
-from tqdm import tqdm
-
-BASE_DATA_DIR = os.path.join("data", "ePillID_data")
-NUM_EPOCHS = 25
-NUM_CLASSES = 4902
-BATCH_SIZE = 5
-
-from datasets import SingleImgPillID
-
-label_encoder = LabelEncoder()
+import src.arguments as arguments
+import src.train_nocv as train_nocv
+from src.metrics import all_avg_precision
 
 
+NUM_CLASSES = 9804
+args = arguments.cv_parser().parse_args()
 
-images_df = pd.read_csv(os.path.join(BASE_DATA_DIR, "all_labels.csv"))
+# TODO: fix path issue when data_root_dir is not aboslute path
+# assert os.path.isabs(args.data_root_dir)
+# find csv files and prepare args for each run
+args.folds_csv_dir = os.path.join(args.data_root_dir, args.folds_csv_dir)
+args.label_encoder = os.path.join(args.folds_csv_dir, "label_encoder.pickle")
 
-label_encoder.fit(images_df['label'])
+# make sure csv_files are sorted
+csv_files = glob(os.path.join(args.folds_csv_dir, "*.csv"))
+args.all_imgs_csv = [x for x in csv_files if x.endswith("all.csv")][0]
 
-transform = transforms.Compose([
-  transforms.ToTensor(),
-  transforms.Resize((224, 224), antialias=True)
-])
+csv_files = sorted([x for x in csv_files if not x.endswith("all.csv")])
 
-train_dataset = SingleImgPillID(df=images_df, label_encoder=label_encoder, transform=transform, train=True)
+args.test_imgs_csv = csv_files.pop(-1)  # use the last fold as hold out
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = vit_b_32(num_classes=NUM_CLASSES)
+print("val csv files: ",csv_files)
 
-optimizer = torch.optim.Adam(model.parameters())
-criterion = nn.CrossEntropyLoss()
+metrics_dfs_list = []
+predictions_dfs_list = []
+for i, val_csv in enumerate(csv_files):
+    args.val_imgs_csv = val_csv
 
-model.to(device)
+    metrics_df, predictions_df = train_nocv.run(args)
+    metrics_dfs_list.append(metrics_df)
+    predictions_dfs_list.append(predictions_df)
 
-for epoch in range(NUM_EPOCHS):
-  running_loss = 0.0
-  for item in tqdm(train_loader):
-    image = item['image'].to(device)
-    label = item['label'].to(device)
-    image_name = item['image_name']
-    is_ref = item['is_ref']
+all_metrics_df = pd.concat(metrics_dfs_list, ignore_index = True)
 
-    optimizer.zero_grad()
-    outputs: torch.Tensor = model(image)
-    
-    loss = criterion(outputs, label)
-    loss.backward()
-    optimizer.step()
-
-    running_loss += loss.item() * image.size(0)
-
-  print(f"epoch {epoch} loss = {running_loss / len(train_loader)}")
+os.makedirs('outputs', exist_ok=True)
+all_metrics_df.to_csv(os.path.join('outputs', 'metrics.csv'))
 
 
+all_predictions_df = pd.concat(predictions_dfs_list, ignore_index = True)
+
+holdout_predictions_df = all_predictions_df[all_predictions_df.dataset == 'holdout'].copy()
+
+precision_metrics = all_avg_precision(all_predictions_df)
+
+plt = precision_metrics['PR-curve']
+plt.close()
+
+num_metrics_df = all_metrics_df[~all_metrics_df.name.str.contains('indices|curve')].copy()
+num_metrics_df["value"] = pd.to_numeric(num_metrics_df["value"], errors='coerce')
+
+agg_metrics_df = num_metrics_df.groupby(['dataset', 'name'])['value'].agg([np.mean, np.std])
+agg_metrics_df.to_csv(os.path.join('outputs', 'agg_metrics.csv'))
+
+all_predictions_df.to_csv(os.path.join('outputs', 'all_eval_predictions.csv'))
 
 
-
-  
